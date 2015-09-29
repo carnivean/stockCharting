781d2235f0e9ed858a89c5bc820cddd77d9bef85
Erik Kynast <erikkynast@gmail.com>
2015-09-29 22:00:55 +0200

    realtime updates across clients works... now we "only" have to do the drawing and stuff

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,7 +5,7 @@ angular.module('stockchartingApp')
     $scope.awesomeThings = [];
 
     $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+      socket.unsyncUpdates('stocks');
     });
 
     // array with all the stocks, we want to check
@@ -15,7 +15,46 @@ angular.module('stockchartingApp')
     $scope.series = [];
     $scope.data = [];
 
+    // init
+    $scope.newStock = '';
+
+    // one var for the apicalls, so we dont have to declare it ten times
+    var apiString;
+
+    // function to get the stocks from the database at the beginning
+    var getStocksFromDb = function () {
+      apiString = '/api/stocks'
+      $http.get(apiString)
+        .success(function(data) {
+            console.log(data);
+
+            // reset the array
+            $scope.stocks = [];
+            for (var index = 0; index < data.length; index++) {
+              $scope.stocks.push(data[index].symbol);
+            }
+
+            socket.syncUpdates('stocks', $scope.stocks);
+        })
+        .error(function(data){
+            console.log('Error: ' + data);
+        })
+    };
+
+    // function to add the stock to the colletion
+    $scope.addStock = function() {
+      if($scope.newStock === '') {
+        return;
+      }
+      $http.post('/api/stocks', {
+        symbol: $scope.newStock
+      });
+      $scope.newStock = '';
+    };
+
     $scope.onClick = function (points, evt) {
       console.log(points, evt);
     };
+
+    getStocksFromDb();
   });
